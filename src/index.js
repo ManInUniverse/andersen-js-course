5439c20687d767ff059604b7e7cc47612f5d559f
Artem Malyutin <malyutinartem92@gmail.com>
2023-01-18 23:28:23 +0300

    Решена задача array-practice/task7

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import forEachRight from './array-practice/task3';
 import union from './array-practice/task4';
 import createGenerator from './array-practice/task5';
 import transformArrayToNumber from './array-practice/task6';
+import take from './array-practice/task7';
 
 console.log(any([0, 1, 2, 0], (x) => x >= 2)); // true
 console.log(any([0, 0, 1, 0])); // true
@@ -61,6 +62,11 @@ console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc + item, 10))
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item)); // 0
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item, 1)); // 6000
 console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc - item)); // -60
+
+console.log('=============');
+
+console.log(take([1, 2, 3], 5)); // [1, 2, 3]
+console.log(take([1, 2, 3], 2)); // [1, 2]
 
 console.log('=============');
 
