bff7e55ff71ea7fcaabc73db222981e5957ca8a8
Artem Malyutin <malyutinartem92@gmail.com>
2023-01-18 22:07:05 +0300

    Решена задача array-practice/task4

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,12 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+export default function union(arr1, arr2) {
+  const set = new Set();
+
+  arr1.forEach((value) => set.add(value));
+  arr2.forEach((value) => set.add(value));
+
+  return Array.from(set);
+}
