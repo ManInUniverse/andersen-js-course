ca76264ffbc0d4ca5caca1c10eb4101b345ef37a
Artem Malyutin <malyutinartem92@gmail.com>
2023-01-19 15:40:21 +0300

    Решена задача es-features/task3

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ import membersOnActiveMeetups from './array-practice/task10';
 
 import { task1Old, task1New } from './es-features/task1';
 import { task2Old, task2New } from './es-features/task2';
+import { task3Old, task3New } from './es-features/task3';
 
 console.log(any([0, 1, 2, 0], (x) => x >= 2)); // true
 console.log(any([0, 0, 1, 0])); // true
@@ -96,7 +97,6 @@ console.log(membersOnActiveMeetups(meetups)); // 1500
 
 console.log('=============');
 
-// import { task3Old, task3New } from './es-features/task3';
 // import { task4Old, task4New } from './es-features/task4';
 // import { task5Old, task5New } from './es-features/task5';
 // import { task6Old, task6New } from './es-features/task6';
@@ -120,10 +120,10 @@ console.log('task2 new', task2New(1, 2, 'test', false, 'new', 'best') === 12); /
 
 console.log('=============');
 
-// console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
-// console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
 
-// console.log('=============');
+console.log('=============');
 
 // console.log('task4 old', task4Old()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
 // console.log('task4 new', task4New()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
