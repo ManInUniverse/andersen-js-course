e279b36da3faecae67de3b920232c734920ca973
Artem Malyutin <malyutinartem92@gmail.com>
2023-01-25 16:18:12 +0300

    Решена задача asynchronous-programming/task-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-console */
 import './styles/main.css';
 
+import { foo, createCb } from './asynchronous-programming/task-1';
+
 console.log('Hi');
+
+console.log('==========');
+
+foo(5, createCb('cb')); // 'x <= 10'
+foo(20, createCb('cb')); // 'x > 10'; 'cb'
+
+console.log('==========');
