18d63cbdd742a5f33266e5b27f76bc7c4610adcb
Artem Malyutin <malyutinartem92@gmail.com>
2023-01-25 21:39:29 +0300

    Решена задача asynchronous-programming/task-6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './styles/main.css';
 import { foo, createCb } from './asynchronous-programming/task-1';
 import { parseJSON, successCb, failureCb } from './asynchronous-programming/task-2';
 import { delay } from './asynchronous-programming/task-3';
+// task-4
+// task-5
+import { getResolvedPromise } from './asynchronous-programming/task-6';
 
 console.log('Hi');
 
@@ -20,5 +23,22 @@ parseJSON('{x}', successCb, failureCb); // 'Failure parse!'; SyntaxError: ...
 console.log('==========');
 
 delay(1000).then((value) => console.log(`Done with ${value}`)); // Done with 100
+
+console.log('==========');
+
+// task-4
+
+// task-5
+
+getResolvedPromise(500)
+  .then((result) => {
+    if (result > 300) {
+      throw new Error('Ошибка');
+    } else {
+      console.log(result);
+    }
+  })
+  .catch((err) => console.log(err.message))
+  .finally(() => console.log('This is Finally!'));
 
 console.log('==========');
